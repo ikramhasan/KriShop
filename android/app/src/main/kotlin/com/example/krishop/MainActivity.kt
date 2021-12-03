61191178ed1a952e746e994fd15e9cce6f1287e1
ikramhasan <ikramhasan.dev@gmail.com>
2021-12-04 01:27:41 +0600

    Initialized: Repo

---
+++ android/app/src/main/kotlin/com/example/krishop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.krishop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:krishop/presentation/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
